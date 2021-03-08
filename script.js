a651b7f13788d2e5f13ac4314a804b7f01f46c2a
SNsaid1998 <61479527+SNsaid1998@users.noreply.github.com>
2021-03-07 20:41:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,87 @@
-require(["esri/config","esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer"], function (esriConfig, Map, MapView, FeatureLayer) {
+require([
+      "esri/Map",
+      "esri/layers/FeatureLayer",
+      "esri/views/MapView",
+      "dojo/domReady!"
+    ], function(
+      Map,
+      FeatureLayer,
+      MapView
+    ) {
 
-esriConfig.apiKey =  "AAPKdc25ad7513a24c46b92cd024b53471173ysex4xHWQwBw2IQ-kNnn5MYzM2Maqnuc7YbUYeHblfF461bYrN1hiCHw4m9SK0E"; 
-
-var map = new Map({
-          basemap: "arcgis-topographic" // Basemap layer service
-        });
-
-var view = new MapView({
-          map: map,
-          center: [-90.35, 38.65], // Longitude, latitude
-          zoom: 11.5, // Zoom level
-          container: "viewDiv" // Div element
-        });
-//STL Parks feature layer (polygons) 
-var parksLayer = new FeatureLayer({
-  url: "https://services9.arcgis.com/l9yXFvhjz46ekkZV/arcgis/rest/services/St_Louis_City_Parks/FeatureServer"
-});
-  
-  map.add(parksLayer);
-          
-//STL Public Schools (points) 
-var pubschoolLayer = new FeatureLayer({
-  url: "https://services2.arcgis.com/bB9Y1bGKerz1PTl5/arcgis/rest/services/Buffer_of_STL_Public_Schools/FeatureServer"
-});
-  
-  map.add(pubschoolLayer);
-  
- });
-
-  var webmap = new WebMap({
-        portalItem: { // autocasts as new PortalItem()
-          id: "13ba212005704b72a72ab1c75903393c"
-        }
+      // Create the map
+      var map = new Map({
+        basemap: "gray"
       });
+
+      // Create the MapView
+      var view = new MapView({
+        container: "viewDiv",
+        map: map,
+        center:[-91.1, 38.6],
+        zoom: 9
+      });
+
+      /*************************************************************
+       * The PopupTemplate content is the text that appears inside the
+       * popup. {fieldName} can be used to reference the value of an
+       * attribute of the selected feature. HTML elements can be used
+       * to provide structure and styles within the content. The
+       * fieldInfos property is an array of objects (each object representing
+       * a field) that is use to format number fields and customize field
+       * aliases in the popup and legend.
+       **************************************************************/
+
+      var template = { // autocasts as new PopupTemplate()
+        title: "Neighborhood: {NHD_NAME}",
+        content: [{
+          // It is also possible to set the fieldInfos outside of the content
+          // directly in the popupTemplate. If no fieldInfos is specifically set
+          // in the content, it defaults to whatever may be set within the popupTemplate.
+          type: "fields",
+          fieldInfos: [{
+            fieldName: "NHD_NUM",
+            label: "Neighborhood Number: ",
+            visible: true
+          },
+                      ]
+        }]
+      };
+
+     var symbol = {
+      type: "picture-marker",  // autocasts as new PictureMarkerSymbol()
+      url: "https://images.app.goo.gl/ue6hSoAbwjuUqYsN9",
+      width: "48px",
+      height: "48px"
+};
+  var renderer = {
+      type: "simple",  // autocasts as new SimpleRenderer()
+      symbol: symbol
+    };
+  
+      // Reference the popupTemplate instance in the
+      // popupTemplate property of FeatureLayer
+      var featureLayer = new FeatureLayer({
+        url: "https://services2.arcgis.com/bB9Y1bGKerz1PTl5/ArcGIS/rest/services/STL_Neighborhood/FeatureServer/0",
+        outFields: ["*"],
+        popupTemplate: template,
+        renderer:renderer
+      });
+  
+      map.add(featureLayer);
+  
+
+   /*
+      featureLayer.renderer = {
+      type: "simple",  // autocasts as new SimpleRenderer()
+      symbol: {
+        type: "simple-marker",  // autocasts as new SimpleMarkerSymbol()
+        size: 6,
+        color: "red",
+        outline: {  // autocasts as new SimpleLineSymbol()
+          width: 0.5,
+          color: "white"
+        }
+      }
+    };*/
+    });
